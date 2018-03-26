38bde0a0e428282cd01d1e7a960304a151182864
kentyku <kentyku@box>
2018-03-26 11:18:33 +0400

    Удалил неиспользуемое

--- src/java/ru/myservlets/MyServlet1.java
+++ src/java/ru/myservlets/MyServlet1.java
@@ -6,9 +6,12 @@ package ru.myservlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,7 +37,7 @@ public class MyServlet1 extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -45,15 +48,21 @@ public class MyServlet1 extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("");
-            out.println("<h1>Servlet MyServlet1 at " + request.getContextPath() + "</h1>");
-            out.println("<h1>Servlet MyServlet1 at " + request.getProtocol() + "</h1>");
+//            out.println("<h1>Servlet MyServlet1 at " + request.getContextPath() + "</h1>");
+//            out.println("<h1>Servlet MyServlet1 at " + request.getProtocol() + "</h1>");
+            
+            
+            //читаем из БД
             ctr=new CountriesTableReader();
-            ctr.readData();
-            countriesList=ctr.getCountriesList();
-            Set<Map.Entry<Integer, String>> countrynames = countriesList.entrySet();//(treemap: ключ=url, значение=site_id)
+            ctr.readData();//читаем
+            countriesList=ctr.getCountriesList();//возвращаем
+            //выводим на экран
+            Set<Map.Entry<Integer, String>> countrynames = countriesList.entrySet();//(treemap: ключ=url, значение=site_id)            
             for (Map.Entry<Integer, String> itemcountry: countrynames) {
-                out.println("<h1>Hi"+itemcountry.getValue()+"</h1>");
-            }          
+                out.println("<h1>"+itemcountry.getValue()+"</h1>");
+            }     
+            
+            
             out.println("</body>");
             out.println("</html>");
         }
@@ -71,7 +80,13 @@ public class MyServlet1 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyServlet1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyServlet1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -85,7 +100,13 @@ public class MyServlet1 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyServlet1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyServlet1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
